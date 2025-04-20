ca142bbae040897c7799ce4298ae4099b369189c
Sagar Nayak <nayaksagar961@gmail.com>
2025-04-20 21:11:05 +0530

    wanderlust

--- WanderLust/models/listing.js
+++ WanderLust/models/listing.js
@@ -2,10 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true,
+    },
     description: String,
-    image: String,
+    image: {
+        type: Object,
+        default: "https://unsplash.com/photos/a-row-of-wooden-cabins-sitting-on-top-of-a-lush-green-field-GOVYM5XUGz0",
+        set: (v) => v === "" ? "https://unsplash.com/photos/a-row-of-wooden-cabins-sitting-on-top-of-a-lush-green-field-GOVYM5XUGz0" : v,
+    },
     price: Number,
     location: String,
     country: String,
 });
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
