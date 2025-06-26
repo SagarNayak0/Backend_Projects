09140fb45fcc74d9dcb127ecd540447a321d93c3
Sagar Nayak <nayaksagar961@gmail.com>
2025-06-26 20:11:23 +0530

    wanderlust

--- WanderLust/models/listing.js
+++ WanderLust/models/listing.js
@@ -28,7 +28,7 @@ listingSchema.post("findOneAndDelete", async (listing) => {
     if(listing) {
         await Review.deleteMany({_id: {$in: listing.reviews}});     
     }
-})
+});
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
